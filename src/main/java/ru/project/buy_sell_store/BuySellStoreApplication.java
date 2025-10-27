c96b9b43d74b77cc070ee193aa75949e8ea900b4
Sapegin Mihail <sapegin2005@yandex.ru>
2025-10-27 14:33:18 +0500

    initial commit

---
+++ src/main/java/ru/project/buy_sell_store/BuySellStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.project.buy_sell_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuySellStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuySellStoreApplication.class, args);
+	}
+
+}
